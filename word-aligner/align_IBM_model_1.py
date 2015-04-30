935f6675fa9d3cb4eecd428d679942e20b8f1125
Seyoung Park <seypak@gmail.com>
2015-04-29 22:02:58 -0700

    small bugs fixed, but it takes forever to run, need to fix more

--- word-aligner/align_IBM_model_1.py
+++ word-aligner/align_IBM_model_1.py
@@ -22,6 +22,7 @@ e_word_count = 0
 g_word_count = 0
 
 #Init
+print 'Initialization'
 for (n, (g, e)) in enumerate(bitext):
     for e_i in set(e):
         if (e_count[e_i] == 1):
@@ -39,23 +40,31 @@ for (n, (g, e)) in enumerate(bitext):
 
 
 # initialize p(e|g)
-for g_word in g_count.keys():
-    for e_word in e_count.keys():
+for (n, (g, e)) in enumerate(bitext):
+    for e_i in set(e):
+        for g_j in set(g):
+            t[e_i][g_j] = 1.0 / e_word_count    
+
+#for g_word in g_count.keys():
+#    for e_word in e_count.keys():
         #Uniform distribution
         #sum_e t(e|g) = 1
-        t[e_word][g_word] = 1.0 / e_word_count    
+#        t[e_word][g_word] = 1.0 / e_word_count    
         
 # initialize q
 #sum_j=0^l q(j|i, l, m) where l is the length of g, m is the length of e
 for (n, (g, e)) in enumerate(bitext):
-    m = length(e)
-    l = length(g)
+    m = len(e)
+    l = len(g)
     for i in xrange(m):
-        for j in xrange(l)+1:
+        for j in xrange(l+1):
             q[j][i][l][m] = (1 / (l+1))
 
+print 'EM'
 #EM
+T = 10
 for t in xrange(T):
+    print t
     # set all counts c(...) = 0
     
     c_2 = defaultdict(float)
@@ -65,19 +74,19 @@ for t in xrange(T):
 
     #set all c_ to zero
     for (k, (g, e)) in enumerate(bitext):
-        m_k = length(e)
-        l_k = length(g)
+        m_k = len(e)
+        l_k = len(g)
 
         for i in xrange(m_k):
-            for j in xrange(l_k)+1:
+            for j in xrange(l_k+1):
                 c_1[g_j][e_i] = 0
                 c_2[g_j] = 0
                 c_3[j][i][l_k][m_k] = 0
-                c_4[i][l_k][m_k] = 0
+                c_4[j][i][m_k] = 0
 
     for (k, (g, e)) in enumerate(bitext):
-        m_k = length(e)
-        l_k = length(g)
+        m_k = len(e)
+        l_k = len(g)
 
 
 
@@ -111,81 +120,24 @@ for t in xrange(T):
                     q[j][i][l][m] = c_3[j][i][l][m] / c_4[i][l][m]
 
 
-'''
+# use params to calculate alignment
+for (k, (g, e)) in bitext :
+    m_k = len(e)
+    l_k = len(g)
 
-# count the number of word
-e_word_count = 0
-f_word_count = 0
-for (n, (f, e)) in enumerate(bitext):
-    for e_i in set(e):
-        if (e_count[e_i] == 1):
-            continue
-        else:
-            e_count[e_i] = 1
-            e_word_count += 1
-
-    for f_i in set(f):
-        if (f_count[f_i] == 1):
-            continue
-        else:
-            f_count[f_i] = 1
-            f_word_count += 1
-
-prob_e_given_g = defaultdict(float)
-
-# initialize p(e|g)
-for e_word in e_count:
-    for g_word in g_count:
-        prob_e_given_g[e_word][g_word] = 1.0 / e_word_count
-
-
-# initialize p(a_i = j)
-for (n, (f, e)) in enumerate(bitext):
-    len_e = len(e) #m : target sentence
-    len_f = len(f) #n : source sentence
-
-    length_pair = str(f_len) + "," + str(e_len)
-
-    # m is target sentence length
-    prod_i = 1;
-    for i in xrange(len_e)
+    for i in xrange(m_k):
         e_word = e[i]
-        for j in xrange(len_f)
-            p_a[length_pair][str(i)][str(j)] = 1 / (1 + float(len_e))
+        #fine a_i = arg max_j
+        a_i = -1
+        best_pr = -99.9
+        for j in xrange(l_k+1):
+            g_word = g[i]
+            pr = q[g_j][e_i][l_k][m_k] * t[e_i][g_j]
 
-# EM
-max_iter = 50
+            if pr > best_pr:
+                best_pr = pr
+                a_i = j
 
-for iter in xrange(max_iter)
-    for
+        print str(e_i) + "-" + str(best_a_pos),;
 
-for i in xrange(m+1)
-
-'''
-'''
-
-    
-    for f_i in set(f):
-        f_count[f_i] += 1
-        for e_j in set(e):
-            fe_count[(f_i,e_j)] += 1
-    for e_j in set(e):
-        e_count[e_j] += 1
-    if n % 500 == 0:
-        sys.stderr.write(".")
-
-    dice = defaultdict(int)
-
-for (k, (f_i, e_j)) in enumerate(fe_count.keys()):
-    dice[(f_i,e_j)] = 2.0 * fe_count[(f_i, e_j)] / (f_count[f_i] + e_count[e_j])
-    if k % 5000 == 0:
-        sys.stderr.write(".")
-sys.stderr.write("\n")
-
-for (f, e) in bitext:
-    for (i, f_i) in enumerate(f): 
-        for (j, e_j) in enumerate(e):
-            if dice[(f_i,e_j)] >= opts.threshold:
-                sys.stdout.write("%i-%i " % (i,j))
-    sys.stdout.write("\n")
-    '''
+    print
